f554f183d339df2bcac232164b0eae6e4748200a
AndresOrtegaGuerrero <oandresg15@gmail.com>
2025-10-19 18:36:47 +0200

    Cleaning & pre-commit

--- examples/PySCF/ch3.py
+++ examples/PySCF/ch3.py
@@ -1,26 +1,24 @@
-'''
+"""
 Testing C++ wrapper for Wannier90
 Hung Q. Pham
 email: pqh3.14@gmail.com
-'''
 
-'''
-a CH3 radical in a box
-Construct four sp3-like WFs from 9 Bloch states
-'''
+Example: a CH3 radical in a box.
+Construct four sp3-like Wannier functions from nine Bloch states.
+"""
 
-import numpy as np
-from pyscf.pbc import gto, dft, df
+from pyscf.pbc import gto, dft
 from pyscf.pbc.tools import pywannier90
+import numpy as np
 
 cell = gto.Cell()
-cell.atom = '''
+cell.atom = """
  C                  3.17500000    3.17500000    3.17500000
  H                  2.54626556    2.54626556    2.54626556
  H                  3.80373444    3.80373444    2.54626556
  H                  2.54626556    3.80373444    3.80373444
-'''
-cell.basis = 'sto-3g'
+"""
+cell.basis = "sto-3g"
 cell.a = np.eye(3) * 6.35
 cell.gs = [15] * 3
 cell.spin = 1
@@ -31,18 +29,17 @@ cell.build()
 nk = [1, 1, 1]
 abs_kpts = cell.make_kpts(nk)
 kmf = dft.KUKS(cell, abs_kpts).mix_density_fit()
-kmf.xc = 'pbe'
+kmf.xc = "pbe"
 ekpt = kmf.run()
 
-num_wann = 4	
-keywords = \
-'''
+num_wann = 4
+keywords = """
 exclude_bands : 1,6-8
 begin projections
 C:sp3
 end projections
-'''
+"""
 
-w90 = pywannier90.W90(kmf, cell, nk, num_wann, other_keywords = keywords)
+w90 = pywannier90.W90(kmf, cell, nk, num_wann, other_keywords=keywords)
 w90.kernel()
-w90.plot_wf(grid=[25,25,25], supercell=nk)
+w90.plot_wf(grid=[25, 25, 25], supercell=nk)
--- examples/PySCF/h2.py
+++ examples/PySCF/h2.py
@@ -1,25 +1,21 @@
-'''
+"""
 Testing C++ wrapper for Wannier90
 Hung Q. Pham
 email: pqh3.14@gmail.com
-'''
+Example: a H2 in a box
+Construct one sigma-like Wannier function from 2 Bloch states
+"""
 
-'''
-a H2 in a box
-Construct one sigma-like WF from 2 Bloch states
-'''
-
-import numpy as np
-from pyscf.pbc import gto, dft, df
+from pyscf.pbc import gto, dft
 from pyscf.pbc.tools import pywannier90
-
+import numpy as np
 
 cell = gto.Cell()
-cell.atom = '''
+cell.atom = """
 H 1.5 1.5 1
 H 1.5 1.5 2
-'''
-cell.basis = '6-31g'
+"""
+cell.basis = "6-31g"
 cell.a = np.eye(3) * 3
 cell.gs = [10] * 3
 cell.verbose = 5
@@ -28,22 +24,21 @@ cell.build()
 nk = [2, 2, 2]
 abs_kpts = cell.make_kpts(nk)
 kmf = dft.KRKS(cell, abs_kpts)
-kmf.xc = 'pbe'
+kmf.xc = "pbe"
 ekpt = kmf.run()
 
 num_wann = 2
-keywords = \
-'''
+keywords = """
 exclude_bands : 3,4
 begin projections
 H:s
 end projections
 wannier_plot = True
 wannier_plot_supercell = 3 3 3
-'''
+"""
 
-w90 = pywannier90.W90(kmf, cell, nk, num_wann, other_keywords = keywords)
+w90 = pywannier90.W90(kmf, cell, nk, num_wann, other_keywords=keywords)
 w90.make_win()
 w90.setup()
-w90.export_unk(grid = [25,25,25])
-w90.kernel() 
+w90.export_unk(grid=[25, 25, 25])
+w90.kernel()
